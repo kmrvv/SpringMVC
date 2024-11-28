67e4e6aa4a91f7e03e2329738a3a156c96d4b2bd
Komarov <kkmrvvv@gmail.com>
2024-11-28 10:50:51 +0300

    Get

--- src/main/java/ru/example/controllers/FirstController.java
+++ src/main/java/ru/example/controllers/FirstController.java
@@ -3,14 +3,29 @@ package ru.example.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+
+        System.out.println(name + " " + surname);
+
         return "first/hello";
     }
+
+//    @GetMapping("/hello")
+//    public String helloPage(HttpServletRequest request) {
+//        String name = request.getParameter("name");
+//        String surname = request.getParameter("surname");
+//
+//        System.out.println(name + " " + surname);
+//
+//        return "first/hello";
+//    }
     @GetMapping("/goodbye")
     public String goodByePage() {
         return "first/goodbye";
