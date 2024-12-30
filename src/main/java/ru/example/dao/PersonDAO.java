4869dd6534df48317d9aea1f26fb40d0a4a9d6fa
Komarov <kkmrvvv@gmail.com>
2024-12-30 17:26:46 +0300

    sql

--- src/main/java/ru/example/dao/PersonDAO.java
+++ src/main/java/ru/example/dao/PersonDAO.java
@@ -1,6 +1,7 @@
 package ru.example.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
@@ -26,11 +27,11 @@ public class PersonDAO {
 
     public Person show(int id) {
         return jdbcTemplate.query("SELECT * FROM person where id=?", new Object[]{id},
-                        new BeanPropertyRowMapper<>(Person.class)).stream().findAny().orElse(null);
+                new BeanPropertyRowMapper<>(Person.class)).stream().findAny().orElse(null);
     }
 
     public void save(Person person) {
-        jdbcTemplate.update("INSERT INTO person VALUES(1, ?, ?, ?)",
+        jdbcTemplate.update("INSERT INTO person(name, age, email) VALUES(?, ?, ?)",
                 person.getName(), person.getAge(), person.getEmail());
     }
 
@@ -42,4 +43,53 @@ public class PersonDAO {
     public void delete(int id) {
         jdbcTemplate.update("DELETE FROM person WHERE id=?", id);
     }
+
+    ////////////// Test package insert (Batch) ///////////////
+    public void testMultipleUpdate() {
+        List<Person> list = create1000Person();
+
+        long before = System.currentTimeMillis();
+        for (Person person : list) {
+            jdbcTemplate.update("INSERT INTO person VALUES(?, ?, ?, ?)",
+                    person.getId(), person.getName(), person.getAge(), person.getEmail());
+        }
+        long after = System.currentTimeMillis();
+
+        System.out.println("Time" + (after-before));
+    }
+
+    public void testBatchUpdate() {
+        List<Person> list = create1000Person();
+
+        long before = System.currentTimeMillis();
+
+        jdbcTemplate.batchUpdate("INSERT INTO person VALUES (?, ?, ?, ?)",
+                new BatchPreparedStatementSetter() {
+                    @Override
+                    public void setValues(PreparedStatement ps, int i) throws SQLException {
+                        ps.setInt(1, list.get(i).getId());
+                        ps.setString(2, list.get(i).getName());
+                        ps.setInt(3, list.get(i).getAge());
+                        ps.setString(4, list.get(i).getEmail());
+                    }
+
+                    @Override
+                    public int getBatchSize() {
+                        return list.size();
+                    }
+                });
+
+        long after = System.currentTimeMillis();
+
+        System.out.println("Time" + (after-before));
+    }
+
+    private List<Person> create1000Person() {
+        List<Person> list = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            list.add(new Person(i, "Name" + i, 15 + i, "test" + i + "@gmail.com"));
+        }
+        return list;
+    }
+
 }
